198167477d444f89ec71e76ef23bb2821502d3e0
liza bogorad dahan <lizabog@gmail.com>
2022-02-15 02:32:55 +0200

    fixed prev tests added tests for document conversion

--- test/index.test.js
+++ test/index.test.js
@@ -1,74 +1,119 @@
 const { expect } = require("chai");
 const { describe } = require("mocha");
 const sinon = require("sinon");
+const axios = require("axios");
 let getDocument;
+let convertDocument;
+let convert;
+const sandbox = sinon.createSandbox();
 
 describe("Fetch data from url", function () {
   describe("data fetched successfully", function () {
     let responseStub;
-    before(function () {
-      responseStub = sinon.stub(getDocument);
-      responseStub.withArgs("http://example.com").returns({
+    beforeEach(function () {
+      responseStub = sinon.stub(axios, "get");
+      responseStub.withArgs("http://example.com").resolves({
         status: 200,
         headers: { "content-type": "text/html; charset=UTF-8" },
       });
+      afterEach(() => {
+        sinon.restore();
+      });
     });
-    it("should have status 200", function () {
-      expect(responseStub.status).to.equal(200);
+    it("should have status 200", async function () {
+      const result = await getDocument("http://example.com");
+
+      expect(result.status).to.equal(200);
     });
-    it("should have header content-type: text/html", function () {
-      expect(responseStub.headers).to.include({
+    it("should have header content-type: text/html", async function () {
+      const result = await getDocument("http://example.com");
+      expect(result).to.include({
         "content-type": "text/html; charset=UTF-8",
       });
     });
   });
   describe("data fetched unsuccessfully", function () {
-    it("should throw TypeError if content type not text/html", function () {
-      const responseStub = sinon.stub(getDocument);
-      responseStub.withArgs("http://example.com").returns({
+    let responseStub;
+    beforeEach(() => {
+      responseStub = sinon.stub(axios, "get");
+    });
+    afterEach(() => {
+      sinon.restore();
+    });
+    it("should throw TypeError if content type not text/html", async function () {
+      responseStub.withArgs("http://example.com").resolves({
         status: 200,
         headers: { "content-type": "application/json" },
       });
-      expect(responseStub).to.Throw(
-        TypeError,
-        "response is not type not text/html"
-      );
-    })
-    it("should throw an error “bad request” If fails with 400 ", function () {
-      const responseStub = sinon.stub(getDocument);
-      responseStub.withArgs("http://example.com").returns({
+      const result = await getDocument("http://example.com");
+      expect(result).to.Throw(TypeError, "response is not type not text/html");
+    });
+    it("should throw an error “bad request” If fails with 400 ", async function () {
+      responseStub.withArgs("http://example.com").resolves({
         status: 400,
       });
-      expect(responseStub).to.Throw("“bad request");
+      const result = await getDocument("http://example.com");
+      expect(result).to.Throw("“bad request");
     });
-  it("should throw unauthorized If fails with 401", function () {
-        const responseStub = sinon.stub(getDocument);
-        responseStub.withArgs("http://example.com").returns({
-          status: 401,
-        });
-        expect(responseStub).to.Throw("Unauthorized");
-  });
-  it("it should  throw “forbidden” If fails with 403", function () {
-    const responseStub = sinon.stub(getDocument);
-    responseStub.withArgs("http://example.com").returns({
-      status: 403,
+    it("should throw unauthorized If fails with 401", function () {
+      responseStub.withArgs("http://example.com").resolves({
+        status: 401,
+      });
+      const result = await getDocument("http://example.com");
+      expect(result).to.Throw("Unauthorized");
     });
-    expect(responseStub).to.Throw("forbidden");
-  });
-  it.skip("it should retry once if still unsuccessful should throw “timeout” If fails with 408", function () {
-    const responseStub = sinon.stub(getDocument);
-    responseStub.withArgs("http://example.com").returns({
-      status: 408,
+    it("it should  throw “forbidden” If fails with 403", function () {
+      responseStub.withArgs("http://example.com").resolves({
+        status: 403,
+      });
+      const result = await getDocument("http://example.com");
+      expect(result).to.Throw("forbidden");
     });
-    expect(responseStub).to.Throw("not found");
+    it.skip("it should retry once if still unsuccessful should throw “timeout” If fails with 408", function () {
+      responseStub.withArgs("http://example.com").resolves({
+        status: 408,
+      });
+    });
+
+    it("should to throw server error if fails with 500s", function () {
+      const responseStub = sinon.stub(getDocument);
+      responseStub.withArgs("http://example.com").resolves({
+        status: 501,
+      });
+      const result = await getDocument("http://example.com");
+      expect(result).to.Throw("server error");
+    });
   });
-  
-   it("should to throw server error if fails with 500s", function () {
-     const responseStub = sinon.stub(getDocument);
-     responseStub.withArgs("http://example.com").returns({
-       status: 501,
-     });
-     expect(responseStub).to.Throw("server error");
-   });
 });
-})
+
+describe("Convert the fetched response", () => {
+  let getDocumentSpy;
+  let convertStub;
+  beforeEach(() => {
+    getDocumentSpy = sinon.spy(getDocument);
+    convertStub = sinon.stub(convert);
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("shouls call fetch once with Url", () => {
+    convertDocument();
+    expect(getDocumentSpy.withArgs("http://example.com").calledOnce).to.be.true;
+  });
+  it("it should call convert after calling getDocument", () => {
+    convertDocument();
+    expect(getDocumentSpy.calledBefore(convertStub)).to.be.true;
+  });
+  it("should throw error “this is not html” received from conversion if called with unexpected", () => {
+    convertStub.throws("this is not html");
+    const result = convertDocument();
+    expect(result).to.throw("this is not html");
+  });
+  it("It should console.log obj received from conversion", () => {
+      const response = { title: "test", body: " test test test test" };
+    convertStub.returns(response);
+    const consoleSpy = sinon.spy(console, "log");
+    convertDocument()
+    expect(consoleSpy.calledWith(response)).to.be.true
+  });
+});
