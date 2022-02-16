9619d3d9cb57d7e9553c843406773c657f1c672d
liza bogorad dahan <lizabog@gmail.com>
2022-02-16 15:45:11 +0200

    fixed tests now axios tested with sinon refactored code

---
+++ index.js
@@ -0,0 +1,55 @@
+const axios = require("axios");
+
+class ConvertHtmlToString {
+  constructor(url) {
+    this.url = url;
+  }
+  async getDocument() {
+    try {
+      const response = await axios.get(this.url);
+      if (response.status !== 200) {
+        throw { status: response.status };
+      }
+      if (response.headers["content-type"] == "text/html; charset=UTF-8") {
+        return response.data;
+      } else {
+        throw new TypeError("not text/html");
+      }
+    } catch (error) {
+      if (error.status && error.status === 400) {
+        throw "bad request";
+      } else if (error.status && error.status === 401) {
+        throw "unauthorized";
+      } else if (error.status && error.status === 403) {
+        throw "forbidden";
+      } else if (error.status && error.status === 404) {
+        throw "not found";
+      } else if (error.status && 499 < error.status < 600) {
+        try {
+          const response = await axios(this.url);
+          if (response.status !== 200) {
+            throw { status: response.status };
+          }
+        } catch (error) {
+          if (error.status && 499 < error.status < 600) {
+            throw "server error";
+          }
+        }
+      } else {
+        throw error;
+      }
+    }
+  }
+  convert(htmlData) {
+    if (!(typeof htmlData === "string" && htmlData.includes("<html>"))) {
+      throw new TypeError ("this is not a valid html");
+    }
+    return {title:"",body:""}
+  }
+  async convertResponse() {
+    const htmlData = await this.getDocument();
+   this.convert(htmlData);
+  }
+}
+
+module.exports = { ConvertHtmlToString };
--- test/index.test.js
+++ test/index.test.js
@@ -1,76 +1,139 @@
-const { expect } = require("chai");
+const { expect, should } = require("chai");
 const { describe } = require("mocha");
 const sinon = require("sinon");
-const axios = require("axios");
-const MockAdapter = require("axios-mock-adapter");
-const chaiAsPromised = require("chai-as-promised");
 const chai = require("chai");
+const sinonChai = require("sinon-chai");
+const axios = require("axios");
+const chaiAsPromised = require("chai-as-promised");
 
-const { exampleHtml } = require("./exampleHtml");
-const { getDocument } = require("../getDocument");
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+const { ConvertHtmlToString } = require("../index");
+const { exampleHtml } = require("./exampleHtml");
+
 
 describe("data fetched successfully ", function () {
   let mockAxios;
+  let convertExample;
+  let axiosStub;
+  before(function () {
+    convertExample = new ConvertHtmlToString("http://example.com");
+  });
   beforeEach(function () {
-    mockAxios = new MockAdapter(axios);
-    mockAxios
-      .onGet("http://example.com")
-      .reply(200, exampleHtml, { "content-type": "text/html; charset=UTF-8" });
+    axiosStub = sinon
+      .stub(axios, "get")
+      .withArgs("http://example.com")
+      .resolves({
+        status: 200,
+        headers: { "content-type": "text/html; charset=UTF-8" },
+        data: exampleHtml,
+      });
   });
   afterEach(() => {
-    mockAxios.restore();
+    sinon.restore();
   });
   it("should return response if status 200 and content type text/html", async function () {
-    const result = await getDocument("http://example.com");
+    const result = await convertExample.getDocument();
     expect(result).to.include("html");
   });
 });
 describe("data fetched unsuccessfully", () => {
   let mockAxios;
+  let axiosStub;
+  let convertExample;
   beforeEach(function () {
-    mockAxios = new MockAdapter(axios);
+    // mockAxios = new MockAdapter(axios);
+    convertExample = new ConvertHtmlToString("http://example.com");
+    axiosStub = sinon.stub(axios, "get");
   });
   afterEach(() => {
-    mockAxios.restore();
+    // mockAxios.restore();
+    sinon.restore();
   });
   it("should throw typeError if content type not text/html ", async () => {
-    mockAxios
-      .onGet("http://example.com")
-      .reply(
-        200,
-        { exampleHtml: "no" },
-        { "content-type": "application/json; charset=UTF-8" }
-      );
+    axiosStub.withArgs("http://example.com").resolves({
+      status: 200,
+      headers: { "content-type": "application/json" },
+    });
 
-    return expect(getDocument("http://example.com")).to.be.rejectedWith(
+    return expect(convertExample.getDocument()).to.be.rejectedWith(
       TypeError,
       "not text/html"
     );
   });
   it("should throw an error “bad request” If status 400", async function () {
-    mockAxios.onGet("http://example.com").reply(400);
-    return expect(getDocument("http://example.com")).to.be.rejectedWith(
+    axiosStub.resolves({ status: 400 });
+
+    return expect(convertExample.getDocument()).to.be.rejectedWith(
       "bad request"
     );
   });
-   it("should throw unauthorized If fails with 401", async function () {
-     mockAxios.onGet("http://example.com").reply(401);
-     return expect(getDocument("http://example.com")).to.be.rejectedWith(
-       "unauthorized"
-     );
-   });
+  it("should throw unauthorized If fails with 401", async function () {
+    axiosStub.resolves({ status: 401 });
 
-   it("should  throw “forbidden” If fails with 403", async function () {
-     mockAxios.onGet("http://example.com").reply(403);
-     return expect(getDocument("http://example.com")).to.be.rejectedWith(
-       "forbidden"
-     );
-   });
-   it("should  throw 'not found' If fails with 404", async function () {
-     mockAxios.onGet("http://example.com").reply(404);
-     return expect(getDocument("http://example.com")).to.be.rejectedWith(
-       "not found"
-     );
-   });
+    return expect(convertExample.getDocument()).to.be.rejectedWith(
+      "unauthorized"
+    );
+  });
+
+  it("should  throw “forbidden” If fails with 403", async function () {
+    axiosStub.resolves({ status: 403 });
+    return expect(convertExample.getDocument()).to.be.rejectedWith("forbidden");
+  });
+  it.skip("should to throw server error if fails with 500s", async function () {
+    axiosStub.resolves({ status: 501 });
+    return expect(convertExample.getDocument()).to.be.rejectedWith(
+      "server error"
+    );
+  });
+});
+
+describe("Convert the fetched response", () => {
+  let getDocumentStub;
+  let convertSpy;
+  let convertExample;
+  let axiosStub;
+
+  beforeEach(() => {
+    axiosStub = sinon.stub(axios, "get").resolves({
+        status: 200,
+        headers: { "content-type": "text/html; charset=UTF-8" },
+        data: exampleHtml,
+      });
+    convertExample = new ConvertHtmlToString("http://example.com");
+
+    getDocumentStub = sinon.stub(convertExample, "getDocument").returns(exampleHtml);
+    convertSpy = sinon.spy(convertExample, "convert");
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("shouls call fetch once ", async () => {
+    await convertExample.convertResponse();
+    expect(getDocumentStub.calledOnce).to.be.true;
+  });
+  it("should call fetch before calling convert", async () => {
+    await convertExample.convertResponse();
+    expect(getDocumentStub.calledBefore(convertSpy)).to.be.true;
+  });
+  it("should throw if getDocument fails", async () => {
+    getDocumentStub.rejects("failed");
+    expect(convertExample.convertResponse()).to.throw;
+  });
+});
+
+describe("test convert method", () => {
+    let convertExample;
+beforeEach(() => {
+    convertExample = new ConvertHtmlToString("http://example.com");
+
+});
+  it("should throw typeError “this is not html” if received data that’s not html", () => {
+   expect(()=>convertExample.convert({ hey: "there" })).throws(TypeError);
+  });
+it ("should return an object containing title and body", () => {
+  const result = convertExample.convert(exampleHtml)
+  expect (result).to.have.all.keys(["title","body"])
+})
 });
